fcdadb7c9372804396a50c0af2abb91bf65f9568
Karim Roy tampubolon <86301908+kajukopi@users.noreply.github.com>
2025-07-17 20:11:41 +0700

    Create firebase.cpp

---
+++ sketch/firebase.cpp
@@ -0,0 +1,34 @@
+#include <FirebaseESP8266.h>
+
+// Firebase credentials
+#define FIREBASE_HOST "payunghitam-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define FIREBASE_EMAIL "esp8266@yourapp.com"
+#define FIREBASE_PASSWORD "password123"
+#define FIREBASE_API_KEY "AIzaSyBczsujBWZbP2eq5C1YR1JF3xPixWVYnxY"
+
+// Global Firebase objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+String deviceStatus = "Unknown";
+
+void initFirebase() {
+  config.api_key = FIREBASE_API_KEY;
+  config.database_url = "https://" FIREBASE_HOST;
+
+  auth.user.email = FIREBASE_EMAIL;
+  auth.user.password = FIREBASE_PASSWORD;
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  deviceStatus = "Online";
+  Firebase.setString(fbdo, "/device/status", deviceStatus);
+}
+
+void logBootTime() {
+  String timeStr = String(millis());
+  String logPath = "/device/logs/" + timeStr;
+  Firebase.setString(fbdo, logPath, "Booted at millis: " + timeStr);
+}
